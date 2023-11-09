fdaf6e4f15102edfb95b47da1450900be8246c4f
mohamedkaram10 <mohamedkaram59999@gmail.com>
2023-11-10 01:04:52 +0200

    Add Roles and Permission For All User

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Permission;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +23,20 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerGates();
     }
+
+    protected function registerGates(): void
+    {
+        try {
+            foreach (Permission::pluck('name') as $permission) {
+                Gate::define($permission, function ($user) use ($permission) {
+                    return $user->hasPermission($permission);
+                });
+            }
+        } catch (\Exception $e) {
+            info('registerPermissions(): Database not found or not yet migrated. Ignoring user permissions while booting app.');
+        }
+    }
+
 }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\RoleName;
+use Illuminate\Database\Seeder;
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Support\Collection;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->createAdminRole();
+    }
+
+    protected function createRole(RoleName $role, Collection $permissions): void
+    {
+        $newRole = Role::create(['name' => $role->value]);
+        $newRole->permissions()->sync($permissions);
+    }
+
+    protected function createAdminRole(): void
+    {
+        $permissions = Permission::query()
+            ->where('name', 'like', 'user.%')
+            ->orWhere('name', 'like', 'restaurant.%')
+            ->pluck('id');
+
+        $this->createRole(RoleName::ADMIN, $permissions);
+    }
+}
